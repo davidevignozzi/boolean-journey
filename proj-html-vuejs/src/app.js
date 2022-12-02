dcaee7ac98bf97cbeb3787cc4ce6f078292941ee
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-12-02 10:29:07 +0100

    :bookmark: proj-html-vuejs

---
+++ proj-html-vuejs/src/app.js
@@ -0,0 +1,76 @@
+var app = new Vue({
+
+    el : '#root',
+
+    data: {
+        navLeftItems: [
+            {
+                class: "fas fa-phone-alt",
+                span: "1.800.567.8910",
+                btn: {
+                    classBtn:"nav-btn",
+                    span:"24hrs"
+                },
+            },
+            {
+                class:"fas fa-envelope",
+                span: "Contact Us"
+            },
+            {
+                class: "fas fa-graduation-cap",
+                span:"Alumni"
+            },
+            {
+                class:"fas fa-landmark",
+                span:"Campus"
+            }
+        ],
+        socialList:[
+            {
+                name: "facebook",
+                class: "fab fa-facebook-f"
+            },
+            {
+                name: "twitter",
+                class: "fab fa-twitter"
+            },
+            {
+                name:"youtube",
+                class:"fab fa-youtube"
+            },
+            {
+                name:"instagram",
+                class:"fab fa-instagram"
+            }
+        ],
+        headerItems:[
+            {content:"HOME",},
+            {
+                content:"ABOUT",
+            },
+            {
+                content:"COURSES",
+                btn: {
+                    classBtn:"btn-hdr",
+                    span:"NEW"
+                },
+            },
+            {content:"EVENTS",},
+            {content:"FACILITIES",},
+            {content:"NEWS",},
+            {
+                content:"ADMISSIONS",
+                btn:{
+                    classBtn: "btn-hdr",
+                    span:"APPLY"
+                }
+            },
+        ],
+        footerItems:["© Theme Fusion","All Right Reserved","Powered by Boolean","support@theme-fusion.com","1.800.555.6789",]  
+    },
+
+    methods: {
+
+    },
+})
+Vue.config.devtools = true
